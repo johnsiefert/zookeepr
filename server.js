b2dcdb344262196c7a1bd0c4309fcf1f05208a62
johnsiefert <johnsief12@gmail.com>
2021-12-06 17:47:38 -0500

    attempt number 2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express ();
 const { animals } = require ('./data/animals');
 
@@ -43,27 +44,6 @@ function filterByQuery(query, animalsArray) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -72,6 +52,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+console.log(`API server now on port ${PORT}!`);
 });
